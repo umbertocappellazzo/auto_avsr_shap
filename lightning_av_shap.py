239c8e3ee82cab536eadc1150ce56ba4317d343a
umbertocappellazzo <umbertocappellazzo@gmail.com>
2026-01-10 10:59:04 +0000

    Check on setting audio and video features to 0.

--- lightning_av_shap.py
+++ lightning_av_shap.py
@@ -102,6 +102,8 @@ class ModelModule(LightningModule):
         assert T_v == T_a, f"Video ({T_v}) and audio ({T_a}) must be temporally aligned!"
         T = T_v
         
+        audio_feat[:, :, :] = 0  # Zero ALL audio features
+        
         # Define feature order (must match concatenation order)
         N_v = T  # Video timesteps (first in concatenation)
         N_a = T  # Audio timesteps (second in concatenation)
